8a776459d36d320df9de3aa080a70373581042f0
pghataorre <pghataorre@and.digital>
2024-08-06 12:22:33 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [result, setResult] = useState('');
+  const [hasResult, setHasResult] = useState(false);
+
+  const handleButtonClick = (event) => {
+    if(hasResult) {
+      setResult(`${event.target.value}`);
+      setHasResult(!hasResult);
+    } else {
+      setResult(result + event.target.value);
+    }
+  }
+
+  const handleButtonClickEquals = () => {
+    setHasResult(!hasResult);
+    setResult(eval(result));
+  }
+
+  const handleButtonClear = () => {
+    setResult('');
+  }
+
+  const handleButtonPercentage = () => {
+    setResult(eval(`${result}/100`));
+    setHasResult(!hasResult);
+  }
+
+  return (
+      <div>
+        <h1>Calculator App Test</h1>
+
+        <div className="calculator-container">
+          <div className="buttons-container-row">
+            <input type="text" value={result} readOnly className="result-pane"/>
+          </div>
+          <div className="buttons-container-row">
+            <button value="C" onClick={(handleButtonClear)}>C</button>   
+            <button value="%" onClick={((e) => handleButtonPercentage(e))}>%</button>      
+          </div>
+          <div className="buttons-container-row">
+            <button value="7" onClick={((e) => handleButtonClick(e))}>7</button>
+            <button value="8" onClick={((e) => handleButtonClick(e))}>8</button>
+            <button value="9" onClick={((e) => handleButtonClick(e))}>9</button>
+            <button value="/" className="operation-button" onClick={((e) => handleButtonClick(e))}>/</button>       
+          </div>
+          <div className="buttons-container-row">
+            <button value="4" onClick={((e) => handleButtonClick(e))}>4</button>
+            <button value="5" onClick={((e) => handleButtonClick(e))}>5</button>
+            <button value="6" onClick={((e) => handleButtonClick(e))}>6</button>
+            <button value="*" className="operation-button" onClick={((e) => handleButtonClick(e))}>*</button>       
+          </div>
+          <div className="buttons-container-row">
+            <button value="1" onClick={((e) => handleButtonClick(e))}>1</button>
+            <button value="2" onClick={((e) => handleButtonClick(e))}>2</button>
+            <button value="3" onClick={((e) => handleButtonClick(e))}>3</button>
+            <button value="-" className="operation-button" onClick={((e) => handleButtonClick(e))}>-</button>       
+          </div>
+          <div className="buttons-container-row">
+            <button value="0" className="zero-button" onClick={((e) => handleButtonClick(e))}>0</button>
+            <button value="." onClick={((e) => handleButtonClick(e))}>.</button>
+            <button value="+" className="operation-button" onClick={((e) => handleButtonClick(e))}>+</button>       
+          </div>
+          <div className="buttons-container-row">
+            <button value="=" className="equals-button operation-button" onClick={((e) => handleButtonClickEquals(e))}>=</button>       
+          </div>
+        </div>  
+      </div>
+  )
+}
+
+export default App
